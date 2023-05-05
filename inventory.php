7eb627213647d59aabc101b66f73b73791a24a6a
Dejan <leopard011817@gmail.com>
2023-05-06 04:40:34 +0900

    update orders and inventory

---
+++ inventory.php
@@ -0,0 +1,11 @@
+<?php
+    require_once(__DIR__ . '/index.php');
+
+    $amazon = new Amazon();
+
+    $currentDateTime = new DateTime('UTC');
+    $endDateTime = $currentDateTime->format('Y-m-d\TH:i:s.000\Z');
+
+    $startDateTime = date('Y-m-d\TH:i:s.000\Z',strtotime('- 15 days'));
+    $amazon->fn_GetInventoryReports($startDateTime, $endDateTime);
+?>
