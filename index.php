782ed89f78b772183d4b9f08ceec19bcca66e2fb
Leopard0817 <leopard011817@gmail.com>
2023-08-17 23:02:08 +0900

    Update all

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+    require_once(__DIR__ . '/config.php');
     require_once(__DIR__ . '/vendor/autoload.php');
 
     use SellingPartnerApi\Configuration;
     use SellingPartnerApi\Endpoint;
     use SellingPartnerApi\Document;
+    use SellingPartnerApi\ReportType;
+    use SellingPartnerApi\FeedType;
     use SellingPartnerApi\Api\SellersV1Api;
     use SellingPartnerApi\Api\OrdersV0Api;
     use SellingPartnerApi\Api\ReportsV20210630Api;
-    use SellingPartnerApi\ReportType;
-    use SellingPartnerApi\FeedType;
     use SellingPartnerApi\Api\FeedsV20210630Api;
     use SellingPartnerApi\Model\FeedsV20210630\CreateFeedDocumentSpecification;
     use SellingPartnerApi\Model\FeedsV20210630\CreateFeedSpecification;
@@ -16,17 +17,6 @@
     use SellingPartnerApi\Model\OrdersV0\ConfirmShipmentRequest;
     use SellingPartnerApi\Model\OrdersV0\PackageDetail;
     use SellingPartnerApi\Model\OrdersV0\ConfirmShipmentOrderItem;
-
-    define('LWA_CLIENT_ID', 'amzn1.application-oa2-client.c61e4e0db5d04b1aae280454412cae23');
-    define('LWA_CLIENT_SECRET', 'amzn1.oa2-cs.v1.9c67e9db746644379b864887d6da051458d57f3d655374c588f3d15d1398fa80');
-    define('LWA_REFRESH_TOKEN', 'Atzr|IwEBIMog3_zCD6umGDnhLthHP6bhPQqRv5eQn3znMSH5bt699WFWrgSX6gHOel8GzqtwV2WwCCv14dFP8M66PAv0s3CQp0kH-jIsP-iDjR-HRycURRzsPSx07pFOhAqBaZQNvRbkCf82nvJo4SC6R5CD9l_i-7NFCvmgEP84FYbT90fa8n_Y4hgyz3z5_xQgRWm2N5BoA14difFTm8ppnXmRAnuQXmuyjIVmXiyMXOFIUcn5Ci-nzhOeuacsDm86NZ_f5U9BWrZFxiVPnQlZYejDnKmjywinFBLVIjsCiVQ1WA4BsFcyNKknz1GCOSy-LyXVG3k');
-
-    define('AWS_ACCESS_KEY_ID', 'AKIAT5MW4DNGXPKSJ3OV');
-    define('AWS_SECRET_KEY', '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');
-
-    define('ROLE_ARN', 'arn:aws:iam::269288741709:role/Developer');
-
-    define('US_MARKETPLACE', 'ATVPDKIKX0DER');
     
     class Amazon {
         private $config;
